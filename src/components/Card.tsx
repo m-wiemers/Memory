0f80d2b1e9fb39950a073ac378c2f3b2e5df344e
Marcel Wiemers <77615352+m-wiemers@users.noreply.github.com>
2022-08-03 09:03:09 +0200

    mobile view (#3)
    Co-authored-by: marcelwiemers <wiemers.marcel@territory.group>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 type Props = {
   src: string;
@@ -8,19 +8,29 @@ type Props = {
 
 const CardWrapper = styled.button`
   background-color: #fff;
-  height: 8rem;
-  width: 8rem;
+  height: 6.5rem;
+  width: 6.5rem;
   cursor: pointer;
   border: none;
+
+  @media screen and (min-width: 480px) {
+    height: 8rem;
+    width: 8rem;
+  }
 `;
 
 const Image = styled.img`
-  max-width: 7rem;
-  max-height: 7rem;
+  max-width: 5.5rem;
+  max-height: 5.5rem;
+
+  @media screen and (min-width: 480px) {
+    max-height: 7rem;
+    max-width: 7rem;
+  }
 `;
 
 const Card = ({ src, onClick, status }: Props): JSX.Element => {
-  const backSrc = "./back.jpg";
+  const backSrc = './back.jpg';
 
   return (
     <CardWrapper onClick={onClick}>
